188c1a188ad658dfc7e57deb747e14eefbb2902f
Evan Machnic <emachnic@broadmac.net>
2011-03-06 19:18:32 -0500

    Modified Day 1

--- Ruby/Day1/day_one.rb
+++ Ruby/Day1/day_one.rb
@@ -1,12 +1,22 @@
-random_number = rand(10)
+random_number = rand(10) + 1
 
-puts 'Pick a number between 0 and 9'
-guess_num = gets.to_i
+begin
+  puts 'Guess a number between 1 and 10'
+  print 'Guess: '
+  user_guess = gets.to_i
 
-if guess_num == random_number
-  puts 'Congratulations!'
-elsif guess_num < random_number
-  puts 'Too low'
-else
-  puts 'Too high'
-end
+  # Get lower and upper range
+  lower_range = (1...random_number)
+  upper_range = ((random_number + 1)..10)
+
+  # Test the user's guess
+  case(user_guess)
+    when lower_range
+      puts 'Too low, guess higher.'
+    when upper_range
+      puts 'Too high, guess lower.'
+  end
+end while user_guess != random_number
+
+# Won't execute until user has guessed the number
+puts "Congratulations, you guessed #{random_number} correctly"
